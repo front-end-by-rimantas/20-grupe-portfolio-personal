5e0817e2aaa69d79de319f2060ef58110ec9072d
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-07 20:50:28 +0200

    removed dropdown hover effects from sidebar, reduced sidebar width

--- js/components/sidebar/openSidebar.js
+++ js/components/sidebar/openSidebar.js
@@ -1,6 +1,6 @@
 function openSidebar() {
     document.getElementById("hamburger").addEventListener('click', function(){
-        document.getElementById("mySidebar").style.width = "300px";
+        document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("mySidebarSection").style.width = "100%";
         document.getElementById("sidebarNav").style.display = "flex";
     })
