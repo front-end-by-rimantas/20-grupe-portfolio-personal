e1e8c7f270f0eebe0f952e90503017734c6e8344
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-08 19:18:35 +0200

    all sidebar dropdown levels functionality/css update

--- js/components/sidebar/openCloseSidebarDropdown.js
+++ js/components/sidebar/openCloseSidebarDropdown.js
@@ -1,10 +1,10 @@
 function openCloseSidebarDropdown() {
     let dropdownBlog = document.querySelectorAll('.dropdown-link-blog');
     let dropdownPages = document.querySelectorAll('.dropdown-link-pages');
+    let dropdownPagesLvl2 = document.querySelectorAll('.pages-level-2-link');
     for (let i = 0; i < dropdownBlog.length; i++){
         dropdownBlog[i].addEventListener('click', function () {
             let dropdownContentBlog = document.querySelector('.sidebar-dropdown-content-blog');
-            console.log(dropdownContentBlog);
             if (dropdownContentBlog.style.display === 'none'){
                 dropdownContentBlog.style.display = 'flex';
             } else {
@@ -16,13 +16,22 @@ function openCloseSidebarDropdown() {
         dropdownPages[i].addEventListener('click', function () {
             let dropdownContentPages = document.querySelector('.sidebar-dropdown-content-pages');
             let dropdownLevel2 = document.querySelector('.sidebar-dropdown-level-2');
-            console.log(dropdownContentPages);
             if (dropdownContentPages.style.display === 'none' && dropdownLevel2.style.display === 'none'){
                 dropdownContentPages.style.display = 'flex';
-                dropdownLevel2.style.display = 'flex';
+                dropdownLevel2.style.display = 'block';
             } else {
                 dropdownContentPages.style.display = 'none';
                 dropdownLevel2.style.display = 'none';
+            }
+        })
+    }
+    for (let i = 0; i < dropdownPagesLvl2.length; i++){
+        dropdownPagesLvl2[i].addEventListener('click', function () {
+            let dropdownLevel2Content = document.querySelector('.sidebar-dropdown-content-lvl-2');
+            if (dropdownLevel2Content.style.display === 'none'){
+                dropdownLevel2Content.style.display = 'block';
+            } else {
+                dropdownLevel2Content.style.display = 'none';
             }
         })
     }
