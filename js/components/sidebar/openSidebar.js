d57ba0176d59ae10bfcdf82d3ee4083a4e56c977
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-08 17:41:18 +0200

    sidebar dropdown css and sidebar js update

--- js/components/sidebar/openSidebar.js
+++ js/components/sidebar/openSidebar.js
@@ -1,9 +1,9 @@
 function openSidebar() {
     document.querySelector("#hamburger").addEventListener('click', function(){
-    document.querySelector("#mySidebar").style.width = "250px";
-    document.querySelector("#mySidebarSection").style.width = "100%";
-    document.querySelector("#sidebarNav").style.display = "flex";
-    document.querySelector("body").style.overflow = "hidden";
+        document.querySelector("#mySidebar").style.width = "250px";
+        document.querySelector("#mySidebarSection").style.width = "100%";
+        document.querySelector("#sidebarNav").style.display = "flex";
+        document.querySelector("body").style.overflow = "hidden";
     })
     return true;
 }
