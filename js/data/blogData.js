b04e253351c752e814498a3edeea15a98e1c110a
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-10 20:54:10 +0200

    blogData and h1 -> h2 in html and css

---
+++ js/data/blogData.js
@@ -0,0 +1,17 @@
+const blogData = [
+    {
+        picture: '/img/blog/pic1.jpg',
+        heading: 'BREAK THROUGH SELF DOUBT AND FEAR',
+        description: 'Dream interpretation has many forms; it can be done be done for the sake of fun, hobby or can be taken up as a serious career.'
+    }
+    {
+        picture: '/img/blog/pic2.jpg',
+        heading: 'PORTABLE FASHION FOR YOUNG WOMEN',
+        description: 'You may be a skillful, effective employer but if you don’t trust your personnel and the opposite, then the chances of improving.'
+    }
+    {
+        picture: '/img/blog/pic3.jpg',
+        heading: 'DO DREAMS SERVE AS A PREMONITION',
+        description: 'So many of us are demotivated to achieve anything. Such people are not enthusiastic about anything. They don’t want to work involved.'
+    }
+]
