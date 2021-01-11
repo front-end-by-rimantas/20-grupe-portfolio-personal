0b6c1ae8fabca663aa562b7a8e7120293f8fc6cc
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-11 17:03:01 +0200

    blogData.js update

--- js/data/blogData.js
+++ js/data/blogData.js
@@ -1,17 +1,23 @@
-const blogData = [
-    {
-        href: '/img/blog/pic1.jpg',
-        heading: 'BREAK THROUGH SELF DOUBT AND FEAR',
-        description: 'Dream interpretation has many forms; it can be done be done for the sake of fun, hobby or can be taken up as a serious career.'
-    },
-    {
-        href: '/img/blog/pic2.jpg',
-        heading: 'PORTABLE FASHION FOR YOUNG WOMEN',
-        description: 'You may be a skillful, effective employer but if you don’t trust your personnel and the opposite, then the chances of improving.'
-    },
-    {
-        href: '/img/blog/pic3.jpg',
-        heading: 'DO DREAMS SERVE AS A PREMONITION',
-        description: 'So many of us are demotivated to achieve anything. Such people are not enthusiastic about anything. They don’t want to work involved.'
-    }
-]
+const blogData = {
+    selector: '',
+    limit: 3,
+    data: [
+        {
+            href: '/img/blog/pic1.jpg',
+            heading: 'BREAK THROUGH SELF DOUBT AND FEAR',
+            description: 'Dream interpretation has many forms; it can be done be done for the sake of fun, hobby or can be taken up as a serious career.'
+        },
+        {
+            href: '/img/blog/pic2.jpg',
+            heading: 'PORTABLE FASHION FOR YOUNG WOMEN',
+            description: 'You may be a skillful, effective employer but if you don’t trust your personnel and the opposite, then the chances of improving.'
+        },
+        {
+            href: '/img/blog/pic3.jpg',
+            heading: 'DO DREAMS SERVE AS A PREMONITION',
+            description: 'So many of us are demotivated to achieve anything. Such people are not enthusiastic about anything. They don’t want to work involved.'
+        }
+    ]
+}
+
+export { blogData }
