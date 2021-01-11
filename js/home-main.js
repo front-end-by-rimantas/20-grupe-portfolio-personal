47385bca16a58f8214a844138618ca7182519562
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-11 19:07:30 +0200

    blog render with js and validation

--- js/home-main.js
+++ js/home-main.js
@@ -20,6 +20,8 @@ import { resultsData } from './data/resultsData.js';
 import { renderPlanPrices} from './components/plans/renderPlanPrices.js';
 import { planPricesData } from './data/planPricesData.js';
 /* Blog */
+import { blogData } from './data/blogData.js';
+import { renderBlog } from './components/blog/renderBlog.js'
 /* Logos */
 /* Footer */
 import { renderSocials } from './components/socials/renderSocials.js';
@@ -28,7 +30,6 @@ import { socialsData } from './data/socialsData.js';
 // CODE EXECUTION
 /* Header */
 /* Sidebar */
-
 openSidebar();
 closeSidebar();
 openCloseSidebarDropdown();
@@ -54,6 +55,11 @@ renderPlanPrices({
     limit: 4,
 })
 /* Blog */
+renderBlog ({
+    selector: '.blog-posts',
+    data: blogData,
+    limit: 3
+})
 /* Logos */
 /* Footer */
 renderSocials ({
